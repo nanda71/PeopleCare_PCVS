278c316db30ecf7552239df99c84de6079fb1b4e
nanda71 <nanda71@protonmail.com>
2021-12-03 18:00:34 +0800

    Collective commits

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,19 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <ul class="nav">
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    {{ __('About Us') }}
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    {{ __('Blog') }}
+                </a>
+            </li>
+        </ul>
+        <div class="copyright">
+            &copy; {{ now()->year }} {{ __('made by Developer Team of PeopleCare PCVS Website') }}
+        </div>
+    </div>
+</footer>

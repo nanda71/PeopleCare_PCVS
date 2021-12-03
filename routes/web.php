4c4bd43b826e4724a7e3f9c334e5b4a24b3bdf9e
nanda71 <nanda71@protonmail.com>
2021-12-03 22:12:40 +0800

    configuration

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Home');
+});
+
+Route::get('/about', function () {
+    return view('About');
+});
+
+Route::get('/blog', function () {
+    return view('Posts');
 });
